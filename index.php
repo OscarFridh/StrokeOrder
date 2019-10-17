0fef86e975f10406a7e9dde35f2d878bc0b1696f
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2019-10-17 18:34:11 +0800

    Read and loop through characters in a text passed with the GET request

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo 'Hello World!';
+
+$text = $_GET['text'];
+if(!$text) {
+    die('No text to display.');
+}
+
+
+// Define a function for splitting a string into unicode characters
+// as the native function str_split does not support unicode.
+// https://www.php.net/manual/en/function.str-split.php
+function str_split_unicode($str, $l = 0) {
+    if ($l > 0) {
+        $ret = array();
+        $len = mb_strlen($str, "UTF-8");
+        for ($i = 0; $i < $len; $i += $l) {
+            $ret[] = mb_substr($str, $i, $l, "UTF-8");
+        }
+        return $ret;
+    }
+    return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+}
+
+
+$characters = str_split_unicode($text);
+foreach ($characters as $character) {
+    echo $character . '<BR>';
+}
+
